e3d86f7b2549d005b0e64ae2d10ae23e944e7add
Timothy Barasa <timbaros87@gmail.com>
2024-05-20 18:38:27 +0300

    Add event handler to toggle cart status

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 
 function Item({ name, category }) {
-const []
+  // Create state variable for the cart status
+  const [inCart, setInCart] = useState(false);
+
+  // Event handler to toggle the cart status
+  const handleCartToggle = () => {
+  setInCart((inCart) => !inCart);
+}
 
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle} > {inCart ? "Remove From Cart" : "Add to Cart"} </button>
     </li>
   );
 }
